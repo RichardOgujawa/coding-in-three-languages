ad55f2242e8b3cf9c07304ce4f588274332d7a72
Richard Ogujawa <richardogujawa7@gmail.com>
2023-11-05 17:17:13 +0000

    fixed error in is-leap-year coding challenge

--- easy/is-leap-year/Python/is_leap_year.py
+++ easy/is-leap-year/Python/is_leap_year.py
@@ -1,6 +1,7 @@
 def is_leap_year(year) -> bool:
     is_leap_year = False
     if not year % 4:
+        is_leap_year = True
         if not year % 400:
             is_leap_year = True
         elif not year % 100:
@@ -9,4 +10,11 @@ def is_leap_year(year) -> bool:
 
 
 if __name__ == "__main__":
+    print(is_leap_year(1800))
     print(is_leap_year(1990))
+    print(is_leap_year(2100))
+    print(is_leap_year(2300))
+    print(is_leap_year(2500))
+    print(is_leap_year(2000))
+    print(is_leap_year(2004))
+    print(is_leap_year(2400))
--- easy/is-leap-year/Python/test/test_is_leap_year.py
+++ easy/is-leap-year/Python/test/test_is_leap_year.py
@@ -2,13 +2,54 @@ from is_leap_year import is_leap_year
 
 
 def main():
+    test_1800_is_not_leap_year()
+    test_1900_is_not_leap_year()
     test_1990_is_not_leap_year()
+    test_2000_is_a_leap_year()
+    test_2004_is_a_leap_year()
+    test_2100_is_not_leap_year()
+    test_2200_is_not_leap_year()
+    test_2300_is_not_leap_year()
+    test_2400_is_a_leap_year()
+    test_2500_is_not_leap_year()
+
+
+def test_1800_is_not_leap_year():
+    # Equivalent to expect().toBeFalsy() in jest
+    assert not is_leap_year(1800)
+
+
+def test_1900_is_not_leap_year():
+    assert not is_leap_year(1900)
 
 
 def test_1990_is_not_leap_year():
-    # Assert that it's falsy
     assert not is_leap_year(1990)
 
 
-if __name__ == "__main__":
-    main()
+def test_2000_is_a_leap_year():
+    assert is_leap_year(2000)
+
+
+def test_2004_is_a_leap_year():
+    assert is_leap_year(2004)
+
+
+def test_2100_is_not_leap_year():
+    assert not is_leap_year(2100)
+
+
+def test_2200_is_not_leap_year():
+    assert not is_leap_year(2200)
+
+
+def test_2300_is_not_leap_year():
+    assert not is_leap_year(2300)
+
+
+def test_2400_is_a_leap_year():
+    assert is_leap_year(2400)
+
+
+def test_2500_is_not_leap_year():
+    assert not is_leap_year(2500)
